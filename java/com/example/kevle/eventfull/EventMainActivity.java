20548d2eaa11563c6d5ec797a5ea0c7fa1ecd473
Kevin L <kevin.leunglh@gmail.com>
2017-03-14 22:34:06 -0400

    Created new fragments for each tab

--- java/com/example/kevle/eventfull/EventMainActivity.java
+++ java/com/example/kevle/eventfull/EventMainActivity.java
@@ -19,6 +19,10 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.example.kevle.fragments.EventBillFragment;
+import com.example.kevle.fragments.EventChatFragment;
+import com.example.kevle.fragments.EventDetailsFragment;
+
 public class EventMainActivity extends AppCompatActivity {
 
     /**
@@ -86,40 +90,7 @@ public class EventMainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_event_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -133,9 +104,14 @@ public class EventMainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return EventDetailsFragment.newInstance();
+                case 1:
+                    return EventChatFragment.newInstance();
+                default:
+                    return EventBillFragment.newInstance();
+            }
         }
 
         @Override
--- java/com/example/kevle/fragments/EventBillFragment.java
+++ java/com/example/kevle/fragments/EventBillFragment.java
@@ -1,25 +1,19 @@
 package com.example.kevle.fragments;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.kevle.adapters.EventListAdapter;
 import com.example.kevle.eventfull.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EventDetailsFragment} interface
- * to handle interaction events.
- * Use the {@link EventDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class EventDetailsFragment extends Fragment {
+public class EventBillFragment extends Fragment {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,19 +23,13 @@ public class EventDetailsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public EventDetailsFragment() {
+    public EventBillFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment EventDetailsFragment.
-     */
     // Can pass in parameters into newInstance() if needed
-    public static EventDetailsFragment newInstance() {
-        EventDetailsFragment fragment = new EventDetailsFragment();
+    public static EventBillFragment newInstance() {
+        EventBillFragment fragment = new EventBillFragment();
         Bundle args = new Bundle();
         return fragment;
     }
@@ -55,7 +43,7 @@ public class EventDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_event_details, container, false);
-        return v;
+        return inflater.inflate(R.layout.fragment_event_bill, container, false);
     }
+
 }
